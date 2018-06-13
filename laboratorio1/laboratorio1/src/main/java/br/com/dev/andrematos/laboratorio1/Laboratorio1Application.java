80d40d2d4e701733dcebea5ad8d122eecbe716a9
Andre Matos <andre.matos@zallpy.com>
2018-06-13 09:37:38 -0300

    Criando uma primeira aplicação com Spring Boot

---
+++ laboratorio1/laboratorio1/src/main/java/br/com/dev/andrematos/laboratorio1/Laboratorio1Application.java
@@ -0,0 +1,12 @@
+package br.com.dev.andrematos.laboratorio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio1Application.class, args);
+	}
+}
