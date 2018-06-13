aee2d78cc4f277144fea6cf08411f5583036937a
Andre Matos <andre.matos@zallpy.com>
2018-06-13 09:47:02 -0300

    Explorando recursos do Spring Boot

---
+++ laboratorio2/src/test/java/br/com/dev/andrematos/laboratorio2/Laboratorio2ApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.dev.andrematos.laboratorio2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Laboratorio2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
